59ad13f7d9d441f0056c4e245045b2e7390b4e2c
Kalleby Melo Moreira <Kalleby@rede.ulbra.br>
2020-03-06 21:09:16 -0300

    Novo computer

--- LPC-6N-master/Projeto21/app_noticias/models.py
+++ LPC-6N-master/Projeto21/app_noticias/models.py
@@ -1,4 +1,28 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+
+
+class Pessoa(models.Model):
+    usuario = models.OneToOneField(User,on_delete = models.CASCADE,
+        verbose_name = 'Usuário')
+    nome = models.CharField("Nome", max_length=128)
+    data_nascimento = models.DateField('Data de Nascimento',blank = True, null = True)
+    telefone_celular = models.CharField("Telefone celular",max_length=15,
+        help_text='Número de telefone celular formato (99) 99999-9999',
+        blank=True, null= True)
+    email = models.EmailField('E-mail',null=True,blank=True)
+
+    def __str__(self):
+        return self.nome
+
+class Tag(models.Model):
+    nome =  models.CharField(max_length=64)
+    slug = models.SlugField(verbose_name="Previa da tag",max_length=64)
+
+    def __str__(self):
+        return self.nome
+
 
 class Noticia(models.Model):
     class Meta:
@@ -6,7 +30,11 @@ class Noticia(models.Model):
         verbose_name_plural = 'Notícias'
     titulo = models.CharField('título',max_length=128, blank=True, null=True)
     conteudo = models.TextField()
-    autor = models.CharField('Autor',max_length=128, blank=True, null=True)
+    autor = models.ForeignKey(Pessoa,on_delete=models.CASCADE,verbose_name='Autor',max_length=128, blank=True, null=True)
     data_publicacao = models.DateField('Data', blank=True, null=True)
+    tags = models.ManyToManyField(Tag)
     def __str__(self):
         return self.titulo
+
+
+
